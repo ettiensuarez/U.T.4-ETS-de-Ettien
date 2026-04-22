163a7f1be5121275bc375518ae0c23f275508ef9
ByFernan72 <218837126+ByFernan72@users.noreply.github.com>
2026-04-22 09:38:21 +0100

    add: clase voto

---
+++ src/Model/Enum/TipoVoto.java
@@ -0,0 +1,8 @@
+package Model.Enum;
+
+public enum TipoVoto {
+    A_FAVOR,
+    EN_CONTRA,
+    ABSTENCION;
+}
+
--- src/Model/Voto.java
+++ src/Model/Voto.java
@@ -1,7 +1,62 @@
 package Model;
 
+import Model.Enum.TipoVoto;
+
 public class Voto {
     private int id;
     private int idUsuario;
     private int idPropuesta;
+    private TipoVoto tipoVoto;
+
+    public Voto() {
+    }
+
+    public Voto(int id, int idUsuario, int idPropuesta, TipoVoto tipoVoto) {
+        this.id = id;
+        this.idUsuario = idUsuario;
+        this.idPropuesta = idPropuesta;
+        this.tipoVoto = tipoVoto;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public int getIdPropuesta() {
+        return idPropuesta;
+    }
+
+    public void setIdPropuesta(int idPropuesta) {
+        this.idPropuesta = idPropuesta;
+    }
+
+    public TipoVoto getTipoVoto() {
+        return tipoVoto;
+    }
+
+    public void setTipoVoto(TipoVoto tipoVoto) {
+        this.tipoVoto = tipoVoto;
+    }
+
+    @Override
+    public String toString() {
+        return "Voto{" +
+                "id=" + id +
+                ", idUsuario=" + idUsuario +
+                ", idPropuesta=" + idPropuesta +
+                ", tipoVoto=" + tipoVoto +
+                '}';
+    }
 }
